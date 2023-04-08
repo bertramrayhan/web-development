79ebf8372667aa968433a27687d0bd2f8a416524
BertramRayhan <21459164-BertramRayhan@users.noreply.replit.com>
2023-04-08 01:07:39 +0000

    add python

--- main.py
+++ main.py
@@ -2,9 +2,24 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+SKILLS = [
+  {
+    "id" : "1.",
+    "Skill" : "Python intermediate"
+  },
+  {
+    "id" : "2.",
+    "Skill" : "Web development beginner"
+  },
+  {
+    "id" : "3.",
+    "Skill" : "Chess"
+  }
+]
+
 @app.route("/")
 def hello():
-  return render_template("home.html")
+  return render_template("home.html", skills = SKILLS)
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", debug=True)
